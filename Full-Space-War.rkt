ee7b6b9d777eac813b9a7b972fc3b3b6749df283
AwadhyM <awadhym@outlook.com>
2022-07-31 11:19:30 +0100

    Update Full-Space-War.rkt
    Added CONSTANTS that will be used throughout the game, and can be adjusted if necessary. These constants represent the different characters of the game, and some of their attributes.

--- Full-Space-War.rkt
+++ Full-Space-War.rkt
@@ -4,3 +4,32 @@
 (require 2htdp/universe)
 (require 2htdp/image)
 
+;; Full-Space-War
+
+;; Constant definitions
+
+(define WIDTH  300)
+(define HEIGHT 500)
+
+(define INVADER-X-SPEED 1.5)  
+(define INVADER-Y-SPEED 1.5)
+(define TANK-SPEED 2)
+(define MISSILE-SPEED 10)
+
+(define HIT-RANGE 10)
+
+(define INVADE-RATE 100)
+
+(define BACKGROUND (rectangle WIDTH HEIGHT "solid" "black"))
+
+(define INVADER
+  (overlay/xy (ellipse 10 15 "outline" "white")              
+              -5 6
+              (ellipse 20 10 "solid"   "white")))
+
+(define TANK
+  (overlay/xy (overlay (ellipse 28 8 "solid" "green")       ;tread center
+                       (ellipse 30 10 "solid" "white"))     ;tread outline
+              5 -14
+              (above (rectangle 5 10 "solid" "green")       ;gun
+                     (rectangle 20 10 "solid" "green"))))   ;main body
